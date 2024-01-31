723aa2e8b8050aa86b5d248f8c5fbe15a34597f4
Abouzar <mortazavi.abouzar@gmail.com>
2024-01-31 22:56:27 +0330

    refactor

--- resources/views/website/payment/error.blade.php
+++ resources/views/website/payment/error.blade.php
@@ -3,7 +3,7 @@
 @section("content")
     <div class="container text-center">
         <h3 class="text-center">عمليات کنسل شد</h3>
-        <a href="/" class="mt-5">
+        <a href="/profile" class="mt-5">
             صفحه اصلی
         </a>
 
