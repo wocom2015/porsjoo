b20bc88e138e5044908f1f0cc8d37f132bdfca38
Abouzar <mortazavi.abouzar@gmail.com>
2024-02-03 07:58:32 +0330

    refactor

--- resources/views/website/payment/error.blade.php
+++ resources/views/website/payment/error.blade.php
@@ -2,9 +2,9 @@
 
 @section("content")
     <div class="container text-center">
-        <h3 class="text-center">عمليات کنسل شد</h3>
-        <a href="/profile" class="mt-5">
-            صفحه اصلی
+        <h3 class="text-center mb-5">عمليات کنسل شد</h3>
+        <a href="/profile" class="default-btn">
+            بازگشت به پروفایل
         </a>
 
     </div>
--- resources/views/website/payment/success.blade.php
+++ resources/views/website/payment/success.blade.php
@@ -2,9 +2,9 @@
 
 @section("content")
     <div class="container text-center">
-        <h3 class="text-center">عمليات با موفقيت انجام شد</h3>
-        <a href="/profile" class="mt-5">
-            صفحه اصلی
+        <h3 class="text-center mb-5">عمليات با موفقيت انجام شد</h3>
+        <a href="/profile" class="default-btn">
+            بازگشت به پروفایل
         </a>
 
     </div>
